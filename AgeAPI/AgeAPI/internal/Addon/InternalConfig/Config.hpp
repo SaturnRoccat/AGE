f81b854af152d9431acb8041fe679614d5412b12
Duckos-Mods <businwes@hotmail.com>
2024-06-23 00:17:11 +0100

    config

---
+++ AgeAPI/AgeAPI/internal/Addon/InternalConfig/Config.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <AgeAPI/internal/Types.hpp>
+
+namespace AgeAPI
+{
+	class Config
+	{
+	public:
+	private:
+		std::string mBasePath;
+		std::string mScriptsPath;
+		std::string mResourcePath;
+		
+		std::string mBehaviorPath;
+		std::string mResourcePath;
+		std::string mArchiveOutputPath;
+	};
+
+}
