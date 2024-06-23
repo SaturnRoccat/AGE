b0a5672cffb261cf559e3b4aa023f271bff16a46
Duckos-Mods <businwes@hotmail.com>
2024-06-23 17:52:45 +0100

    texture stuff

---
+++ AgeAPI/AgeAPI/internal/BackendGeneration/RP/Textures/Texture.cpp
@@ -0,0 +1,167 @@
+#include "Texture.hpp"
+#include <fstream>
+#include <filesystem>
+#include <endianness.h>
+
+namespace AgeAPI::Backend::Rp
+{
+
+	TextureLayer::TextureLayer(const std::string& path, bool lazyLoad) : mIsLazyLoaded(lazyLoad)
+	{
+		if (lazyLoad)
+			mPath = path;
+		else
+			readToMemory(path);
+
+	}
+	void TextureLayer::Write(const std::string& path)
+	{
+		if (mIsLazyLoaded) // If lazy loaded we can just copy the file with no read to memory
+		{
+			std::filesystem::path sourcePath = mPath;
+			std::filesystem::path destPath = path;
+
+			try // TODO: Change to Error as value
+			{
+				std::filesystem::copy_file(sourcePath, destPath, std::filesystem::copy_options::overwrite_existing);
+			}
+			catch (std::exception& e)
+			{
+				throw std::runtime_error("Failed to copy file: " + std::string(e.what()));
+			}
+			return;
+		}
+
+		std::vector<std::unique_ptr<u8[]>> fileData;
+		switch (mBitDepth)
+		{
+		case 8:
+			fileData = std::move(this->writeNBit<u8>());
+			break;
+		case 16:
+			fileData = std::move(this->writeNBit<u16>());
+			break;
+		}
+
+		FILE* file = fopen(path.c_str(), "wb");
+		if (!file)
+			throw std::runtime_error("Failed to open file: " + path);
+
+		png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+		if (!png)
+			throw std::runtime_error("Failed to create png write struct");
+
+		png_infop info = png_create_info_struct(png);
+		if (!info)
+			throw std::runtime_error("Failed to create png info struct");
+
+		if (setjmp(png_jmpbuf(png)))
+			throw std::runtime_error("Failed to set jump buffer");
+
+		png_init_io(png, file);
+		png_set_IHDR(
+			png,
+			info,
+			mSize.x,
+			mSize.y,
+			mBitDepth,
+			mColorType,
+			mInterlacing,
+			mCompressionType,
+			mFilterType
+		);
+
+		png_write_info(png, info);
+		png_set_swap(png);
+
+		png_write_image(png, (u8**)fileData.data());
+		png_write_end(png, nullptr);
+
+		png_destroy_write_struct(&png, &info);
+		fclose(file);
+	}
+	void TextureLayer::Resize(IVec2 newSize)
+	{
+		handleLazyWrite();
+		TextureLayer newSelf(newSize, mBitDepth, mColorType, mInterlacing, mFilterType, mCompressionType);
+		auto newY = std::min(mSize.y, newSize.y);
+		auto newX = std::min(mSize.x, newSize.x);
+		for (int y = 0; y < newY; y++)
+			for (int x = 0; x < newX; x++)
+				newSelf[{x, y}] = (*this)[{x, y}];
+		*this = std::move(newSelf);
+	}
+	void TextureLayer::readToMemory(const std::string& path)
+	{
+		FILE* file = fopen(path.c_str(), "rb"); // Dislike using C functions but tbh i dont wanna deal with figuring out how to get lib png to work with them
+		if (!file)
+			throw std::runtime_error("Failed to open file: " + path);
+
+		png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+		if (!png)
+			throw std::runtime_error("Failed to create png read struct");
+
+		png_infop info = png_create_info_struct(png);
+		if (!info)
+			throw std::runtime_error("Failed to create png info struct");
+
+		if (setjmp(png_jmpbuf(png)))
+			throw std::runtime_error("Failed to set jump buffer");
+
+		png_init_io(png, file);
+		png_read_info(png, info);
+
+		mSize.x = png_get_image_width(png, info);
+		mSize.y = png_get_image_height(png, info);
+		mColorType = png_get_color_type(png, info);
+		mBitDepth = png_get_bit_depth(png, info);
+		mInterlacing = png_get_interlace_type(png, info);
+		mFilterType = png_get_filter_type(png, info);
+		mCompressionType = png_get_compression_type(png, info);
+		if (mBitDepth > 16)
+			throw std::runtime_error("Bit depth is too high");
+		mData.resize(mSize.x * mSize.y * sizeof(float)); // 4 bytes per pixel
+
+
+		png_set_swap(png);
+
+		if (mColorType == PNG_COLOR_TYPE_PALETTE)
+			png_set_palette_to_rgb(png);
+
+		if (mColorType == PNG_COLOR_TYPE_GRAY && mBitDepth < 8)
+			png_set_expand_gray_1_2_4_to_8(png);
+
+		if (png_get_valid(png, info, PNG_INFO_tRNS))
+			png_set_tRNS_to_alpha(png);
+
+		if (mColorType == PNG_COLOR_TYPE_RGB || mColorType == PNG_COLOR_TYPE_GRAY || mColorType == PNG_COLOR_TYPE_PALETTE)
+			png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+		if (mColorType == PNG_COLOR_TYPE_GRAY || mColorType == PNG_COLOR_TYPE_GRAY_ALPHA)
+			png_set_gray_to_rgb(png);
+
+		//png_set_swap(png);
+		png_read_update_info(png, info);
+
+		std::vector<std::unique_ptr<u8[]>> data(mSize.y);
+		for (int i = 0; i < mSize.y; i++)
+			data[i] = std::unique_ptr<u8[]>(new u8[png_get_rowbytes(png, info)]);
+
+		png_read_image(png, (u8**)data.data()); // A little hacky but it works :3
+
+		switch (mBitDepth)
+		{
+		case 8:
+			this->readNBit<u8>(data);
+			break;
+		case 16:
+			// Demon shit lmao
+			this->readNBit<u16>(*reinterpret_cast<std::vector<std::unique_ptr<u16[]>>*>(&data));
+			break;
+		}
+
+		png_destroy_read_struct(&png, &info, nullptr);
+
+
+	}
+}
---
+++ AgeAPI/AgeAPI/internal/BackendGeneration/RP/Textures/Texture.hpp
@@ -0,0 +1,227 @@
+#pragma once
+#include <png.h>
+#include <AgeAPI/internal/Types.hpp>
+#include <algorithm>
+
+namespace AgeAPI::Backend::Rp
+{
+	struct Color
+	{
+		float r, g, b, a;
+
+		Color operator*(float factor) const { return { r * factor, g * factor, b * factor, a * factor }; }
+		Color operator*(const Color& other) const { return { r * other.r, g * other.g, b * other.b, a * other.a }; }
+		Color operator+(const Color& other) const { return { r + other.r, g + other.g, b + other.b, a + other.a }; }
+		Color operator-(const Color& other) const { return { r - other.r, g - other.g, b - other.b, a - other.a }; }
+		Color operator/(float factor) const { return { r / factor, g / factor, b / factor, a / factor }; }
+		Color operator/(const Color& other) const { return { r / other.r, g / other.g, b / other.b, a / other.a }; }
+		Color operator-() const { return { -r, -g, -b, -a }; }
+		Color& operator*=(float factor) { r *= factor; g *= factor; b *= factor; a *= factor; return *this; }
+		Color& operator*=(const Color& other) { r *= other.r; g *= other.g; b *= other.b; a *= other.a; return *this; }
+		Color& operator+=(const Color& other) { r += other.r; g += other.g; b += other.b; a += other.a; return *this; }
+		Color& operator-=(const Color& other) { r -= other.r; g -= other.g; b -= other.b; a -= other.a; return *this; }
+		Color& operator/=(float factor) { r /= factor; g /= factor; b /= factor; a /= factor; return *this; }
+		Color& operator/=(const Color& other) { r /= other.r; g /= other.g; b /= other.b; a /= other.a; return *this; }
+		
+	};
+
+	class TextureLayer
+	{
+	public:
+		TextureLayer(const std::string& path, bool lazyLoad = true); // By default we only load into memory on write and if we never write we just copy from the disk 
+		TextureLayer(u8 colorType, u8 bitDepth, u8 interlacing, u8 filterType, u8 compressionType, const std::vector<Color>& data, IVec2 size) : mColorType(colorType), mBitDepth(bitDepth), mInterlacing(interlacing), mFilterType(filterType), mCompressionType(compressionType), mData(data), mSize(size) {}
+		TextureLayer(
+			IVec2 size,
+			u8 bitDepth = 8,
+			u8 colorType = PNG_COLOR_TYPE_RGBA,
+			u8 interlacing = PNG_INTERLACE_NONE,
+			u8 filterType = PNG_FILTER_TYPE_BASE,
+			u8 compressionType = PNG_COMPRESSION_TYPE_BASE
+		) : mSize(size), mBitDepth(bitDepth), mColorType(colorType), mInterlacing(interlacing), mFilterType(filterType), mCompressionType(compressionType), mData(size.x * size.y) {}
+
+		TextureLayer() = default;
+
+		void Write(const std::string& path);
+
+		Color& operator[](IVec2 pos) { return mData[pos.x + pos.y * mSize.x]; }
+		Color& operator[](i32 index) { return mData[index]; }
+
+		const Color& operator[](IVec2 pos) const { return mData[pos.x + pos.y * mSize.x]; }
+		const Color& operator[](i32 index) const { return mData[index]; }
+
+		Color& At(IVec2 pos) { return mData[pos.x + pos.y * mSize.x]; }
+		Color& At(i32 index) { return mData[index]; }
+
+		const Color& At(IVec2 pos) const { return mData[pos.x + pos.y * mSize.x]; }
+		const Color& At(i32 index) const { return mData[index]; }
+
+		void Fill(Color color) { handleLazyWrite();  std::fill(mData.begin(), mData.end(), color); }
+		void FillRow(i32 row, Color color) { handleLazyWrite(); std::fill(mData.begin() + row * mSize.x, mData.begin() + (row + 1) * mSize.x, color); }
+		void FillColumn(i32 column, Color color) { handleLazyWrite(); for (i32 y = 0; y < mSize.y; y++) At({ column, y }) = color; }
+		
+		void Multiply(Color color) 
+		{ 
+			handleLazyWrite();
+			std::transform(mData.begin(), mData.end(), mData.begin(), [&color](Color c) { return c * color; }); 
+		}
+		void MultiplyRow(i32 row, Color color) {
+			handleLazyWrite();
+			std::transform(
+				mData.begin() + row * mSize.x,
+				mData.begin() + (row + 1) * mSize.x, mData.begin() + row * mSize.x,
+				[&color](Color c) { return c * color;});
+		}
+		void MultiplyColumn(i32 column, Color color) {
+			handleLazyWrite();
+			for (i32 y = 0; y < mSize.y; y++)
+			{
+				At({ column, y }) *= color;
+			}
+		}
+		void Multiply(const TextureLayer& other) {
+			handleLazyWrite();
+			for (i32 i = 0; i < mData.size(); i++)
+				mData[i]*= other[i];
+		}
+		void Multiply(const TextureLayer& other, float factor) {
+			handleLazyWrite();
+			for (i32 i = 0; i < mData.size(); i++)
+				mData[i]*= other[i] * factor;
+		}
+
+
+		IVec2 GetSize() const { return mSize; }
+		i32 GetWidth() const { return mSize.x; }
+		i32 GetHeight() const { return mSize.y; }
+		u8 GetBitDepth() const { return mBitDepth; }
+		u8 GetColorType() const { return mColorType; }
+		u8 GetInterlacing() const { return mInterlacing; }
+		u8 GetFilterType() const { return mFilterType; }
+		u8 GetCompressionType() const { return mCompressionType; }
+
+		void Resize(IVec2 newSize);
+		void Resize(i32 newWidth, i32 newHeight) { Resize(IVec2{ newWidth, newHeight }); }
+
+		void SetColorType(u8 colorType) { mColorType = colorType; }
+		void SetBitDepth(u8 bitDepth) { mBitDepth = bitDepth; }
+		void SetInterlacing(u8 interlacing) { mInterlacing = interlacing; }
+		void SetFilterType(u8 filterType) { mFilterType = filterType; }
+		void SetCompressionType(u8 compressionType) { mCompressionType = compressionType; }
+		void SafeSet(IVec2 pos, Color color) 
+		{
+			BoundingBox<IVec2> b = BoundingBox<IVec2>{ {0, 0}, mSize };
+			if (!b.Contains(pos)) [[unlikely]]
+				return;
+
+			auto& col = At(pos);
+			col = color;
+		}
+	private:
+		void readToMemory(const std::string& path);
+		void handleLazyWrite() {
+			if (mIsLazyLoaded) [[unlikely]]
+			{
+				mIsLazyLoaded = false;
+				readToMemory(mPath);
+			}
+		}
+		template<std::integral T>
+		void readNBit(const std::vector<std::unique_ptr<T[]>>& pixelData)
+		{
+			mData.resize(mSize.x * mSize.y);
+			for (i32 y = 0; y < mSize.y; y++)
+			{
+				const T* pixelRow = reinterpret_cast<const T*>(pixelData[y].get());
+				for (i32 x = 0; x < mSize.x; x++)
+				{
+					auto* colorDataOfRow = pixelRow + x * 4; 
+					// Assuming RGBA format which can be upheld due to libpng conversion
+					Color& color = mData[x + y * mSize.x];
+					color.r = static_cast<float>(colorDataOfRow[0]) / std::numeric_limits<T>::max();
+					color.g = static_cast<float>(colorDataOfRow[1]) / std::numeric_limits<T>::max();
+					color.b = static_cast<float>(colorDataOfRow[2]) / std::numeric_limits<T>::max();
+					color.a = static_cast<float>(colorDataOfRow[3]) / std::numeric_limits<T>::max();
+				}
+			}
+		}
+
+		template<std::integral T>
+		std::vector<std::unique_ptr<u8[]>> writeNBit()
+		{
+			auto BaseWrite = [&]<size_t colorCount>(){
+				std::vector<std::unique_ptr<T[]>> pixelData;
+				pixelData.resize(mSize.y);
+				for (i32 y = 0; y < mSize.y; y++)
+				{
+					void* data = new T[mSize.x * colorCount];
+#ifndef NDEBUG
+					memset(data, 0xAB, mSize.x * colorCount);
+#endif
+					pixelData[y] = std::unique_ptr<T[]>((T*)data);
+					for (i32 x = 0; x < mSize.x; x++)
+					{
+						std::unique_ptr<T[]>& pixelRow = pixelData[y];
+						std::array<T, colorCount>* colorDataOfRow = ((std::array<T, colorCount>*)pixelRow.get()) + x; // RGBA format this needs to be updated lol
+						std::array<float, colorCount>* colorData = (std::array<float, colorCount>*) & mData[x + y * mSize.x];
+						std::transform(
+							colorData->begin(),
+							colorData->end(),
+							colorDataOfRow->begin(),
+							[](float val)
+							{ return (T)(val * (float)std::numeric_limits<T>::max()); }
+						);
+
+
+
+					}
+				}
+				return std::move(*(std::vector<std::unique_ptr<u8[]>>*) & pixelData);
+			};
+			switch (this->mColorType)
+			{
+			case PNG_COLOR_TYPE_RGBA:
+				return BaseWrite.operator()<4>();
+			case PNG_COLOR_TYPE_RGB:
+				return BaseWrite.operator()<3>();
+			case PNG_COLOR_TYPE_GRAY:
+				return BaseWrite.operator()<1>();
+			case PNG_COLOR_TYPE_GRAY_ALPHA:
+				return BaseWrite.operator()<2>();
+			default:
+				throw std::runtime_error("Unsupported color type");
+			}
+				
+		}
+	private:
+		bool mIsLazyLoaded = false;
+		std::string mPath = "";
+		u8 mColorType{};
+		u8 mBitDepth{};
+		u8 mInterlacing{};
+		u8 mFilterType{};
+		u8 mCompressionType{};
+		std::vector<Color> mData{};
+		IVec2 mSize{ 0, 0 };
+
+
+	};
+
+	
+
+	class Texture
+	{
+	private:
+		constexpr static u8 mFilterType = PNG_FILTER_TYPE_BASE;
+		constexpr static u8 mCompressionType = PNG_COMPRESSION_TYPE_BASE;
+		constexpr static u8 mInterlacing = PNG_INTERLACE_NONE;
+
+		std::vector<TextureLayer> mLayers{ 4 };
+		IVec2 mSize;
+		u8 mBitDepth;
+		u8 mColorType;
+
+
+
+	};
+
+}
